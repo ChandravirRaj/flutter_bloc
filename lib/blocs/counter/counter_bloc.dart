6d94e1b7f8ade83d37e2d70ba356eb9f5bc2ef8d
chandravir <chandravir.singh@mobcoder.com>
2024-12-22 23:10:08 +0530

    add copyWith method in state class

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -3,26 +3,28 @@ import 'package:fultter_bloc_pattern/blocs/counter/counter_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterBloc extends Bloc<CounterEvents, CounterState> {
-
   CounterBloc()
       : super(const CounterState(
             counterCurrentValue: 0, message: "Initial State")) {
+    on<IncrementCounterEvent>(_increment);
+    on<DecrementCounterEvent>(_decrement);
+    on<ResetCounterEvent>(_reset);
+  }
 
-    on<IncrementCounterEvent>((event, emit) {
-      emit(CounterState(
-          counterCurrentValue: state.counterCurrentValue + 1,
-          message: "Counter value incremented by one"));
-    });
+  void _increment(IncrementCounterEvent event, Emitter emitter) {
+    emitter(CounterState(
+        counterCurrentValue: state.counterCurrentValue + 1,
+        message: "Counter value incremented by one"));
+  }
 
-    on<DecrementCounterEvent>((event, emit) {
-      emit(CounterState(
-          counterCurrentValue: state.counterCurrentValue - 1,
-          message: "Counter is decremented by one"));
-    });
+  void _decrement(DecrementCounterEvent event, Emitter emitter) {
+    emitter(CounterState(
+        counterCurrentValue: state.counterCurrentValue - 1,
+        message: "Counter is decremented by one"));
+  }
 
-    on<ResetCounterEvent>((event, emit) {
-      emit(const CounterState(
-          counterCurrentValue: 0, message: 'Reset to Zero!'));
-    });
+  void _reset(ResetCounterEvent event, Emitter emitter) {
+    emitter(
+        const CounterState(counterCurrentValue: 0, message: 'Reset to Zero!'));
   }
 }
--- lib/blocs/counter/counter_states.dart
+++ lib/blocs/counter/counter_states.dart
@@ -1,11 +1,15 @@
 import 'package:equatable/equatable.dart';
 
 class CounterState extends Equatable {
-
   final int counterCurrentValue;
   final String message;
 
-  const CounterState({required this.counterCurrentValue, required this.message});
+  const CounterState({this.counterCurrentValue = 0, required this.message});
+
+  CounterState copyWith({int? counter}) {
+    return CounterState(
+        counterCurrentValue: counter ?? counterCurrentValue, message: message);
+  }
 
   @override
   List<Object?> get props => [counterCurrentValue, message];
